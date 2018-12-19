c8608d4ebde2dfa8f386eac0dbfd7d602eb8a1e0
microparcel <admin@microparcel.co>
2018-12-19 21:53:15 +0530

    Initial commit

---
+++ app/src/main/java/co/microparcel/mp_bookings/SplashActivity.java
@@ -0,0 +1,13 @@
+package co.microparcel.mp_bookings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
