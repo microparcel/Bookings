d837ff07bddf287c3173461f905966fc4393651d
microparcel <admin@microparcel.co>
2018-12-20 10:25:32 +0530

    Commit for sahilsingh at 20-12-2018:10:25

---
+++ app/src/main/java/co/microparcel/mp_bookings/LoginActivity.java
@@ -0,0 +1,13 @@
+package co.microparcel.mp_bookings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
